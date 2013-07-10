fe0f960a4d2f78835f3a09fe2b28d463eb8adbbb
SergioKch <geresy.vorchuko@gmail.com>
2013-07-10 16:07:59 +0000

    Update web.js

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ var buffer = new Buffer("Something wrong..", "utf-8");
 app.get('/', function(request, response) {
     fs.readFile('index.html', function(err, data) {
     if (err) throw err;
-    buffer.write(data); 
+    buffer.write(data, "utf-8"); 
     });
 
   response.send(buffer.toString()); 
