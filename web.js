3fdd59a047e7e813d41842ea3bc4542c42d0453a
SergioKch <geresy.vorchuko@gmail.com>
2013-07-10 16:41:31 +0000

    Update web.js

--- web.js
+++ web.js
@@ -4,18 +4,15 @@ var fs = require('fs');
  
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer("Something wrong", "utf-8");
+var buffer = new Buffer(30);
 
 var data = '';
 
 app.get('/', function(request, response) {
-    //fs.readFileSync('index.html', function(err, data) {
-    //if (err) throw err;
+
     data = fs.readFileSync('index.html', 'utf-8');
     buffer.write(data, "utf-8"); 
-    //});
-
-  response.send(buffer.toString('utf-8')); 
+    response.send(buffer.toString('utf-8')); 
 });
 
 var port = process.env.PORT || 5000;
